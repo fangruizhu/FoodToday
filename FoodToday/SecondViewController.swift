dc2f68760629bbf6b6c2d779094f03bf10bc2db4
zjzsliyang <zjzsliyang@gmail.com>
2016-09-20 06:41:07 +0800

    Initial Commit

---
+++ FoodToday/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FoodToday
+//
+//  Created by Yang on 20/09/2016.
+//  Copyright © 2016 Yang Li. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
